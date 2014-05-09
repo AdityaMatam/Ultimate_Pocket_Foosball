6c925de5744f560cc785fce9e12988097a7b9296
Blockgen <ilia_p@live.com>
2014-05-08 21:05:21 -0400

    game stuff

--- src/upf/Game1.java
+++ src/upf/Game1.java
@@ -2,39 +2,50 @@ package upf;
 
 //Imports
 import java.awt.*;
+
 import javax.swing.*;
+
 import java.awt.event.*;
 import java.net.URL;
 
-public class Menu implements ActionListener
+public class Game1 implements ActionListener
 {
 	// Declarations
 	Font title = new Font("Berlin Sans FB Demi", Font.PLAIN, 42);
 	Font buttonBig = new Font("Eras Demi ITC", Font.PLAIN, 25);
-	static JLabel sBackground;
-	static JLabel title1, title2, title3 = new JLabel("Foosball", SwingConstants.CENTER);
-	static JButton play1, play2;
-	
-	public Menu() {// Constructor (Output)
+	Font adFont = new Font("Arial", Font.PLAIN, 20);
+	static JButton play1, play2, ad;
+
+	public Game1() {// Constructor (Output)
 		
 		// Background
-		sBackground = new JLabel(new ImageIcon("resources/menuBKG.jpg"));
+		JLabel sBackground = new JLabel(new ImageIcon("resources/gameBKG.jpg"));
 		sBackground.setBounds(14, 46, 253, 445);
 
+		//Net
+		ImageIcon nett = new ImageIcon("resources/goalnett.png");
+		JLabel net1 = new JLabel(nett);
+		net1.setBounds(100,78,82,36);
+		ImageIcon netb = new ImageIcon("resources/goalnetb.png");
+		JLabel net2 = new JLabel(netb);
+		net2.setBounds(100,424,82,36);
+
+		
+		/*
 		// Ultimate
-		title1 = new JLabel("Ultimate", SwingConstants.CENTER);
+		JLabel title1 = new JLabel("Ultimate", SwingConstants.CENTER);
 		title1.setBounds(14, 75, 253, 42);
 		title1.setFont(title);
 		title1.setForeground(Color.WHITE);
 
 		// Pocket
-		title2 = new JLabel("Pocket", SwingConstants.CENTER);
+		JLabel title2 = new JLabel("Pocket", SwingConstants.CENTER);
 		title2.setBounds(14, 125, 253, 42);
 		title2.setFont(title);
 		title2.setForeground(Color.WHITE);
 
 		// Foosball
-		title3 = new JLabel("Foosball", SwingConstants.CENTER);
+		JLabel title3 = new JLabel("Foosball", SwingConstants.CENTER);
 		title3.setBounds(14, 175, 253, 42);
 		title3.setFont(title);
 		title3.setForeground(Color.WHITE);
@@ -50,17 +61,17 @@ public class Menu implements ActionListener
 		play2.setFont(buttonBig);
 		play2.setBounds(24, 310, 233, 40);
 		play2.addActionListener(this);
-		
-		// Ad button
-		UPF.ad.addActionListener(this);
 
+		// AD Button
+		ad = new JButton("advertisement");
+		ad.setFont(adFont);
+		ad.setBounds(14, 452, 253, 40);
+		ad.addActionListener(this);
+		*/
 		// Adding
 		UPF.lp.add(sBackground, new Integer(1));
-		UPF.lp.add(title1, new Integer(2));
-		UPF.lp.add(title2, new Integer(2));
-		UPF.lp.add(title3, new Integer(2));
-		UPF.lp.add(play1, new Integer(2));
-		UPF.lp.add(play2, new Integer(2));
+		UPF.lp.add(net1, new Integer(2));
+		UPF.lp.add(net2, new Integer(2));
 		
 		UPF.f.repaint();
 
@@ -71,38 +82,19 @@ public class Menu implements ActionListener
 	{
 		// Processing
 		if (e.getSource() == play1)
-		{	
-			UPF.lp.remove(sBackground);
-			UPF.lp.remove(title1);
-			UPF.lp.remove(title2);
-			UPF.lp.remove(title3);
-			UPF.lp.remove(play1);
-			UPF.lp.remove(play2);
-			UPF.f.repaint();
-
-			new Game1();
-
-		}
-		if (e.getSource() == play2)
 		{
-			try
-			{
-				Desktop.getDesktop().browse(
-						new URL("http://heyyeyaaeyaaaeyaeyaa.com/").toURI());
-			} catch (Exception a)
-			{
-			}
+			UPF.f.dispose();
+
 		}
 		if (e.getSource() == UPF.ad)
 		{
 			try
 			{
 				Desktop.getDesktop().browse(
-						new URL("http://www.theuselessweb.com/").toURI());
+						new URL("http://omfgdogs.com/").toURI());
 			} catch (Exception a)
 			{
 			}
 		}
-
 	}
 }
--- src/upf/UPF.java
+++ src/upf/UPF.java
@@ -1,13 +1,49 @@
 package upf;
 
+import java.awt.Font;
+
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 
 public class UPF {
 
-	static ImageIcon phone = new ImageIcon("resources/phone.png");
-
+	//static ImageIcon phone = new ImageIcon("resources/phone.png");
+	static JFrame f = new JFrame("Ultimate Pocket Foosball");
+	static JLayeredPane lp = new JLayeredPane();
+	static JButton ad = new JButton("advertisement");
+	static Font adFont = new Font("Arial", Font.PLAIN, 20);
+	
 	public static void main(String[] args) {
+		ImageIcon phone = new ImageIcon("resources/phone.png");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setResizable(false);
+		f.setBounds(200, 100, 280 + 6, 550 + 25);
+		f.setLayout(null);
+		
+		// Phone
+		JLabel pBackground = new JLabel(phone);
+		pBackground.setSize(280, 550);
+		
+		// AD Button
+		ad = new JButton("advertisement");
+		ad.setFont(adFont);
+		ad.setBounds(14, 452, 253, 40);
+		
+		// Layered pane
+		lp.setBounds(0, 0, 280, 550);
+		lp.add(pBackground, new Integer(4));
+		UPF.lp.add(ad, new Integer(2));
+		
+		UPF.f.add(UPF.lp);
+		UPF.f.setVisible(true);
+		
 		new Menu();
+		
+		
 	}
-
+	
+	
 }
